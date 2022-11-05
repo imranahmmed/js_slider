3b27988203180e024203af845055d2e50a8d896b
@imranahamed <imranahamed733@gmail.com>
2022-11-05 16:50:55 +0600

    next and prev slide

--- script.js
+++ script.js
@@ -5,6 +5,20 @@ console.log(slidesArr)
 function nextPrev() {
     let currentSlide = document.querySelector(".slide.active");
     let currentIndex = slidesArr.indexOf(currentSlide);
-    console.log(currentIndex)
+
+    let prev;
+    let next;
+
+    if (currentIndex == 0) {
+        prev = slidesArr[slidesArr.length - 1];
+    } else {
+        prev = slidesArr[currentIndex - 1];
+    }
+    if (currentIndex == slidesArr.length - 1) {
+        next = slidesArr[0];
+    } else {
+        next = slidesArr[currentIndex + 1];
+    }
+    return next, prev
 }
-nextPrev()
+nextPrev();
